982d2cd56fa0fc78fa83ef440d8b0e2c02995443
Olivia <omm19@txstate.edu>
2024-06-24 23:01:08 -0400

    cleaned up code, added comments

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// load a websites.html template on startup
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('websites.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('contentContainer').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading content:', error));
+});
+
+// load a specified template
+function loadContent(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('contentContainer').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading content:', error));
+}
+
+// Activate '+' and '-' buttons in experience section
+document.addEventListener('DOMContentLoaded', () => {
+const jobItems = document.querySelectorAll('.job-item');
+jobItems.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('active');
+        });
+    });
+});
